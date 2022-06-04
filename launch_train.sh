c1a3ba46d4edac47dd512b2839cb2c4c8d3380a5
antragoudaras <antonistragoudaras@gmail.com>
2022-06-04 18:03:08 +0300

    update launc_train script

--- launch_train.sh
+++ launch_train.sh
@@ -11,6 +11,6 @@ SRC_DIR="$PROJECT_DIR"
 JOB_RESULTS_DIR="$PROJECT_DIR"/results
 mkdir -p "$JOB_RESULTS_DIR"
 
-JOB_NAME=continue-uNAS-cnn_mnist_struct_pru
+JOB_NAME=continue_36h-uNAS-cnn_mnist_struct_pru
 
-sbatch --job-name "$JOB_NAME" "$SRC_DIR"/train.sbatch --load-from /ibex/scratch/tragoua/tensorflow-uNAS/uNAS/artifacts/cnn_mnist/12hours_uNAS_agingevosearch_state.pickle --name "$JOB_NAME"
+sbatch --job-name "$JOB_NAME" "$SRC_DIR"/train.sbatch --load-from /ibex/scratch/tragoua/tensorflow-uNAS/uNAS/artifacts/cnn_mnist/continue-uNAS-cnn_mnist_struct_pru_agingevosearch_state..pickle --name "$JOB_NAME"
