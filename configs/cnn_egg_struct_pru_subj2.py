4795fcd26cd795e7a76bc4ac1db61dc0be41d389
antragoudaras <antonistragoudaras@gmail.com>
2022-06-15 17:12:56 +0300

    added struct_pru for eeg subject 2

---
+++ configs/cnn_egg_struct_pru_subj2.py
@@ -0,0 +1,10 @@
+from config import PruningConfig
+from configs.cnn_egg_aging_subj2 import training_config, bound_config, search_config, search_algorithm
+
+training_config.pruning = PruningConfig(
+    structured=True,
+    start_pruning_at_epoch=10,
+    finish_pruning_by_epoch=57,
+    min_sparsity=0.05,
+    max_sparsity=0.8
+)
