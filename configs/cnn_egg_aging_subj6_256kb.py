71f6ea20a132d6143dcbe0f021b0da9feb2147cb
antragoudaras <antonistragoudaras@gmail.com>
2022-06-16 20:29:47 +0300

    changes for 256kb peak mem usage SRAM

---
+++ configs/cnn_egg_aging_subj6_256kb.py
@@ -0,0 +1,23 @@
+from tensorflow_addons.optimizers import AdamW
+
+from dataset import EggDataset
+from config import TrainingConfig, BoundConfig
+from configs.cnn_egg_aging_base import search_config, search_algorithm
+
+training_config = TrainingConfig(
+    training_config = TrainingConfig(
+    dataset=EggDataset(subject_id=6, validation_split=0.2),
+    epochs=75,
+    batch_size=26,
+    optimizer=lambda: AdamW(learning_rate=0.0005, weight_decay=1e-5),
+    callbacks=lambda: [],
+)
+)
+print('Tragos training config, train_values: {}, train_shape: {}'.format(training_config.dataset.train[0], training_config.dataset.train[0].shape), flush=True)
+
+bound_config = BoundConfig(
+    error_bound=0.2,
+    peak_mem_bound=256000,
+    model_size_bound=160000,
+    mac_bound=200000000
+)
---
+++ launch_train_aging_evo_subj1_256kb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# entire script fails if a single command fails
+set -e
+
+# script should be run from the project directory
+PROJECT_DIR="$PWD"
+
+# project should have a src directory
+SRC_DIR="$PROJECT_DIR"
+JOB_RESULTS_DIR="$PROJECT_DIR"/results_egg_subject_id1_constrained_256_kb
+mkdir -p "$JOB_RESULTS_DIR"
+
+JOB_NAME=uNAS_egg_dataset_subject_id1_no_pruning_new_setup_256kb
+
+sbatch "$SRC_DIR"/train_egg_aging_evo_subj1_256kb.sbatch --name "$JOB_NAME"
