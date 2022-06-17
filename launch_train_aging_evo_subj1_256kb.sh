4451b42d51fa17386fb3a2235dc20759754a81fd
antragoudaras <antonistragoudaras@gmail.com>
2022-06-17 16:11:57 +0300

    130 epochs uNAS search 80-20 split subj1

--- launch_train_aging_evo_subj1_256kb.sh
+++ launch_train_aging_evo_subj1_256kb.sh
@@ -8,9 +8,9 @@ PROJECT_DIR="$PWD"
 
 # project should have a src directory
 SRC_DIR="$PROJECT_DIR"
-JOB_RESULTS_DIR="$PROJECT_DIR"/results_egg_subject_id1_constrained_256_kb
+JOB_RESULTS_DIR="$PROJECT_DIR"/results_egg_subject_id1_constrained_256_kb_130_epochs_20_percent
 mkdir -p "$JOB_RESULTS_DIR"
 
-JOB_NAME=uNAS_egg_dataset_subject_id1_no_pruning_new_setup_256kb
+JOB_NAME=uNAS_egg_dataset_subject_id1_no_pruning_new_setup_256kb_130_epochs_20_percent
 
 sbatch "$SRC_DIR"/train_egg_aging_evo_subj1_256kb.sbatch --name "$JOB_NAME"
