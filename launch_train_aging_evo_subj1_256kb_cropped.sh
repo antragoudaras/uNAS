e4b1346c90dcfb8d8312db161a30f437432330bb
antragoudaras <antonistragoudaras@gmail.com>
2022-06-19 00:11:09 +0300

    load from script to continue cropped training

--- launch_train_aging_evo_subj1_256kb_cropped.sh
+++ launch_train_aging_evo_subj1_256kb_cropped.sh
@@ -9,8 +9,9 @@ PROJECT_DIR="$PWD"
 # project should have a src directory
 SRC_DIR="$PROJECT_DIR"
 JOB_RESULTS_DIR="$PROJECT_DIR"/results_egg_cropped_subject_id1_constrained_256_kb_130_epochs_20_percent
+LOAD_FROM = "$PROJECT_DIR"/artifacts/cnn_egg_cropped/uNAS_egg_dataset_cropped_subject_id1_no_pruning_new_setup_256kb_130_epochs_20_percent_agingevosearch_state.pickle
 mkdir -p "$JOB_RESULTS_DIR"
 
 JOB_NAME=uNAS_egg_dataset_cropped_subject_id1_no_pruning_new_setup_256kb_130_epochs_20_percent
 
-sbatch "$SRC_DIR"/train_egg_aging_evo_subj1_256kb_cropped.sbatch --name "$JOB_NAME"
+sbatch "$SRC_DIR"/train_egg_aging_evo_subj1_256kb_cropped.sbatch --name "$JOB_NAME" --load-from "$LOAD_FROM"
