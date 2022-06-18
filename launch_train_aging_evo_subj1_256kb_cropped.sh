5b347b42c1e4eb39af35e997e4115c0ec1f9f182
antragoudaras <antonistragoudaras@gmail.com>
2022-06-19 00:15:51 +0300

    removed gaps for LOAD_FROM

--- launch_train_aging_evo_subj1_256kb_cropped.sh
+++ launch_train_aging_evo_subj1_256kb_cropped.sh
@@ -9,7 +9,7 @@ PROJECT_DIR="$PWD"
 # project should have a src directory
 SRC_DIR="$PROJECT_DIR"
 JOB_RESULTS_DIR="$PROJECT_DIR"/results_egg_cropped_subject_id1_constrained_256_kb_130_epochs_20_percent
-LOAD_FROM = "$PROJECT_DIR"/artifacts/cnn_egg_cropped/uNAS_egg_dataset_cropped_subject_id1_no_pruning_new_setup_256kb_130_epochs_20_percent_agingevosearch_state.pickle
+LOAD_FROM="$PROJECT_DIR"/artifacts/cnn_egg_cropped/uNAS_egg_dataset_cropped_subject_id1_no_pruning_new_setup_256kb_130_epochs_20_percent_agingevosearch_state.pickle
 mkdir -p "$JOB_RESULTS_DIR"
 
 JOB_NAME=uNAS_egg_dataset_cropped_subject_id1_no_pruning_new_setup_256kb_130_epochs_20_percent
