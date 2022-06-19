89549c71d627148339c565a1ced41ac7da76f1d9
antragoudaras <antonistragoudaras@gmail.com>
2022-06-19 20:07:43 +0300

    correction

--- configs/cnn_egg_aging_subj1_512kb_20perc.py
+++ configs/cnn_egg_aging_subj1_512kb_20perc.py
@@ -1,13 +1,13 @@
 from tensorflow_addons.optimizers import AdamW
 
-from dataset import EggDatasetCropped
+from dataset import EggDataset
 from config import TrainingConfig, BoundConfig
-from configs.cnn_egg_aging_base_cropped import search_config, search_algorithm
+from configs.cnn_egg_aging_base import search_config, search_algorithm
 
 training_config = TrainingConfig(
-    dataset=EggDatasetCropped(subject_id=1, validation_split=0.2),
+    dataset=EggDataset(subject_id=1, validation_split=0.2),
     epochs=130,
-    batch_size=26,
+    batch_size=87,
     optimizer=lambda: AdamW(learning_rate=0.0005, weight_decay=1e-5),
     callbacks=lambda: [],
 )
@@ -15,8 +15,8 @@ training_config = TrainingConfig(
 print('Tragos training config, train_values: {}, train_shape: {}'.format(training_config.dataset.train[0], training_config.dataset.train[0].shape), flush=True)
 
 bound_config = BoundConfig(
-    error_bound=0.2,
-    peak_mem_bound=256000,
-    model_size_bound=160000,
-    mac_bound=200000000
+    error_bound=0.05,
+    peak_mem_bound=512000,
+    model_size_bound=320000,
+    mac_bound=400000000
 )
