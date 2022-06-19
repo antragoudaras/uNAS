423830e020744a2fd7624d1c6087985601004803
antragoudaras <antonistragoudaras@gmail.com>
2022-06-19 20:47:12 +0300

    512KB 130 epochs 10% split cropped

---
+++ launch_train_aging_evo_subj1_512kb_cropped_10perc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# entire script fails if a single command fails
+set -e
+
+# script should be run from the project directory
+PROJECT_DIR="$PWD"
+
+# project should have a src directory
+SRC_DIR="$PROJECT_DIR"
+JOB_RESULTS_DIR="$PROJECT_DIR"/results_egg_subject_id1_cropped_constrained_512_kb_130_epochs_10_percent
+mkdir -p "$JOB_RESULTS_DIR"
+
+JOB_NAME=uNAS_egg_dataset_cropped_subject_id1_no_pruning_new_setup_512kb_130_epochs_10_percent
+
+sbatch "$SRC_DIR"/train_egg_aging_evo_subj1_512kb_cropped_10perc.sbatch --name "$JOB_NAME"
