5e09e9f5ec829801f9ba4582b97a29bd035e1480
antragoudaras <antonistragoudaras@gmail.com>
2022-06-19 19:59:19 +0300

    512kb 130 epochs 30% split cropped

--- launch_train_aging_evo_subj1_512kb_cropped_30perc.sh
+++ launch_train_aging_evo_subj1_512kb_cropped_30perc.sh
@@ -8,9 +8,9 @@ PROJECT_DIR="$PWD"
 
 # project should have a src directory
 SRC_DIR="$PROJECT_DIR"
-JOB_RESULTS_DIR="$PROJECT_DIR"/results_egg_subject_id1_cropped_constrained_512_kb_75_epochs_30_percent
+JOB_RESULTS_DIR="$PROJECT_DIR"/results_egg_subject_id1_cropped_constrained_512_kb_130_epochs_30_percent
 mkdir -p "$JOB_RESULTS_DIR"
 
-JOB_NAME=uNAS_egg_dataset_cropped_subject_id1_no_pruning_new_setup_512kb_75_epochs_30_percent
+JOB_NAME=uNAS_egg_dataset_cropped_subject_id1_no_pruning_new_setup_512kb_130_epochs_30_percent
 
 sbatch "$SRC_DIR"/train_egg_aging_evo_subj1_512kb_cropped.sbatch --name "$JOB_NAME"
