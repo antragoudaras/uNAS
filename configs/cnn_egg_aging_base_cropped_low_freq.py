d36df71aea624009d16ff990847c138e7e59ee74
antragoudaras <antonistragoudaras@gmail.com>
2022-06-23 20:50:43 +0300

    512 KB 10% split cropped low freq

---
+++ configs/cnn_egg_aging_base_cropped_low_freq.py
@@ -0,0 +1,13 @@
+from cnn import CnnSearchSpace
+from config import AgingEvoConfig
+
+from search_algorithms import AgingEvoSearch
+
+search_algorithm = AgingEvoSearch
+
+search_config = AgingEvoConfig(
+    search_space=CnnSearchSpace(),
+    population_size=100,
+    rounds=4000,
+    checkpoint_dir="artifacts/cnn_egg_cropped_low_freq"
+)
--- dataset/__init__.py
+++ dataset/__init__.py
@@ -6,4 +6,6 @@ from .dummy import Dummy
 from .chars74k import Chars74K
 from .speech_commands import SpeechCommands
 from .egg_dataset import EggDataset
+from .egg_dataset_low_freq import EggDatasetLowFreq
 from .egg_dataset_cropped import EggDatasetCropped
+from .egg_dataset_cropped_low_freq import EggDatasetCroppedLowFreq
