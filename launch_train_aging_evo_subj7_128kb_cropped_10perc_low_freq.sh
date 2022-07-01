825daaa0499be29b2041a9003c7be8e90e1dd27f
antragoudaras <antonistragoudaras@gmail.com>
2022-07-01 21:26:28 +0300

    remaining subject low freq

---
+++ launch_train_aging_evo_subj7_128kb_cropped_10perc_low_freq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# entire script fails if a single command fails
+set -e
+
+# script should be run from the project directory
+PROJECT_DIR="$PWD"
+
+# project should have a src directory
+SRC_DIR="$PROJECT_DIR"
+JOB_RESULTS_DIR="$PROJECT_DIR"/results_egg_low_freq_subject_id7_cropped_constrained_128_kb_70_epochs_10_percent
+mkdir -p "$JOB_RESULTS_DIR"
+
+JOB_NAME=uNAS_egg_dataset_low_freq_cropped_subject_id7_no_pruning_new_setup_128kb_70_epochs_10_percent
+
+sbatch "$SRC_DIR"/train_egg_aging_evo_subj7_128kb_cropped_10perc_low_freq.sbatch --name "$JOB_NAME"
