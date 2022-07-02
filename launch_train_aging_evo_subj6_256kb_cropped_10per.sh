cc40d8b58be5cd39566cc1814eb034026f0bb1a6
antragoudaras <antonistragoudaras@gmail.com>
2022-07-02 18:09:30 +0300

    rerun2 subj6 high freq

--- launch_train_aging_evo_subj6_256kb_cropped_10per.sh
+++ launch_train_aging_evo_subj6_256kb_cropped_10per.sh
@@ -8,9 +8,9 @@ PROJECT_DIR="$PWD"
 
 # project should have a src directory
 SRC_DIR="$PROJECT_DIR"
-JOB_RESULTS_DIR="$PROJECT_DIR"/rerun_results_egg_cropped_subject_id6_constrained_256_kb_130_epochs_10_percent
+JOB_RESULTS_DIR="$PROJECT_DIR"/rerun2_results_egg_cropped_subject_id6_constrained_256_kb_130_epochs_10_percent
 mkdir -p "$JOB_RESULTS_DIR"
 
-JOB_NAME=rerun_uNAS_egg_dataset_cropped_subject_id6_no_pruning_new_setup_256kb_130_epochs_10_percent
+JOB_NAME=rerun2_uNAS_egg_dataset_cropped_subject_id6_no_pruning_new_setup_256kb_130_epochs_10_percent
 
 sbatch "$SRC_DIR"/train_egg_aging_evo_subj6_256kb_cropped_10perc.sbatch --name "$JOB_NAME" --load-from "$SRC_DIR"/artifacts/cnn_egg_cropped/rerun_uNAS_egg_dataset_cropped_subject_id6_no_pruning_new_setup_256kb_130_epochs_10_percent_agingevosearch_state.pickle
